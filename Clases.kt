27afc7edaae6ec37f86798e7318251a5fe7f3f8f
faruckchavarria <faruckchavarria2@gmail.com>
2020-11-26 15:49:52 -0600

    se genera el primer ejecutable y se compila y se guardan los cambios

---
+++ Clases.kt
@@ -0,0 +1,14 @@
+class ImpresoraHola(var tam:Int){
+    fun imprimirHola() {
+    println("Hola!")
+    }
+}
+   
+fun main(){
+    val impresora = ImpresoraHola(3)
+    println(impresora.tam)
+    impresora.imprimirHola() // imprime Hola!
+
+}
+
+   
